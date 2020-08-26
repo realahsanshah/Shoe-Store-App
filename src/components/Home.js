f285c952a3b13b31c29f01468f7f526bf864598b
ahsanshah <shahahsan56@gmail.com>
2020-08-26 22:03:59 +0500

    Shoe Store App v1.0

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 
 const Home = () => {
     return ( 
         <div>
-            <h1>Home</h1>
-            <button className='btn btn-primary'>Hello</button>
+            {/* <h1 className=' ml-auto'>Welcome to Shoe Store</h1> */}
+            <img className='mt-5' src='https://www.iconspng.com/images/nike-logo.jpg' alt='logo' /> 
         </div>
      );
 }
