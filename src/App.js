d3647aaf679f4832387d2187c617702e9ba8ddac
ahsanshah <shahahsan56@gmail.com>
2020-08-26 18:48:20 +0500

    Basic setup 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import {Routes,Route} from 'react-router-dom';
 import Header from './components/header.js';
+import Home from './components/home.js';
 
 function App() {
   return (
     <div>
       <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
       
     </div>
   );
