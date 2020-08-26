e3bdab308811f24d3638b46ae7d9031b3064faf3
ahsanshah <shahahsan56@gmail.com>
2020-08-26 20:51:10 +0500

    Added Product and Product Details

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,10 +1,63 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {useParams} from 'react-router-dom';
+import {Card,CardContent} from '@material-ui/core';
+
+
+const shoes = {
+    "air-jordan-3-valor-blue": {
+      name: "VALOUR BLUE",
+      img:"https://secure-images.nike.com/is/image/DotCom/CT8532_104_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+      price:"$2499"
+    
+    },
+    "jordan-mars-270-london": {
+      name: "JORDAN MARS 270 LONDON",
+      img:"https://secure-images.nike.com/is/image/DotCom/CV3042_001_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+      price:"$2199"
+    },
+    "air-jordan-1-zoom-racer-blue": {
+      name: "RACER BLUE",
+      img:"https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+      price:"$1999"
+    },
+    "nike-blazer":{
+        name:"Nike Blazer Mid",
+        img:"https://static.nike.com/a/images/f_auto/q_auto:eco/t_PDP_864_v1/eric5lwitzffpoisq0rj/blazer-mid-77-vintage-shoe-flCCX4.jpg",
+        price:"$999"
+    },
+    "nike-air-force1":{
+        name:"Nike Air Force 1 '07'",
+        img:"https://static.nike.com/a/images/f_auto/q_auto:eco/t_PDP_864_v1/ndbgikrmsa0ejcdmkvpg/air-force-1-07-mens-shoe-TjqcX1.jpg",
+        price:"$900"
+    },
+    "kyrie-low-3":{
+        name:'Kyrie Low 3 "Eclipse"',
+        img:"https://static.nike.com/a/images/f_auto/q_auto:eco/t_PDP_864_v1/87b514a5-1ca4-4d36-8e37-31835f440124/kyrie-low-3-eclipse-basketball-shoe-4VT0rq.jpg",
+        price:"$1100"
+    }
+  };
 
 const ProductDetails = () => {
     const {productId}=useParams();
+    const selectedShoe=shoes[productId]
+    
+    const [quantity,setQuantity]=useState(1);   
+
+    if(!selectedShoe){
+        return <h3>Shoe not available</h3>
+    }
     return ( 
-        <h4>Product details of {productId}</h4>
+        <div>
+            <h3>Product Details</h3>
+            <Card className='col-12' variant='outlined'>
+                    <CardContent>
+                        <h4>{selectedShoe.name}</h4>
+                        <img src={selectedShoe.img} alt={selectedShoe.name} style={{width:'24rem'}} />
+                        <h5>Price: {selectedShoe.price}</h5>
+                        <button className="btn btn-primary">Add to Cart</button>
+                    </CardContent>
+            </Card>
+        </div>
      );
 }
  
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,12 +1,71 @@
 import React from 'react'
-import {Outlet} from 'react-router-dom';
+import {Outlet,useNavigate} from 'react-router-dom';
+import {Card,CardContent,makeStyles} from '@material-ui/core';
 
+const shoes = {
+    "air-jordan-3-valor-blue": {
+      name: "VALOUR BLUE",
+      img:"https://secure-images.nike.com/is/image/DotCom/CT8532_104_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+      price:"$2499"
+    
+    },
+    "jordan-mars-270-london": {
+      name: "JORDAN MARS 270 LONDON",
+      img:"https://secure-images.nike.com/is/image/DotCom/CV3042_001_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+      price:"$2199"
+    },
+    "air-jordan-1-zoom-racer-blue": {
+      name: "RACER BLUE",
+      img:"https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+      price:"$1999"
+    },
+    "nike-blazer":{
+        name:"Nike Blazer Mid",
+        img:"https://static.nike.com/a/images/f_auto/q_auto:eco/t_PDP_864_v1/eric5lwitzffpoisq0rj/blazer-mid-77-vintage-shoe-flCCX4.jpg",
+        price:"$999"
+    },
+    "nike-air-force1":{
+        name:"Nike Air Force 1 '07'",
+        img:"https://static.nike.com/a/images/f_auto/q_auto:eco/t_PDP_864_v1/ndbgikrmsa0ejcdmkvpg/air-force-1-07-mens-shoe-TjqcX1.jpg",
+        price:"$900"
+    },
+    "kyrie-low-3":{
+        name:'Kyrie Low 3 "Eclipse"',
+        img:"https://static.nike.com/a/images/f_auto/q_auto:eco/t_PDP_864_v1/87b514a5-1ca4-4d36-8e37-31835f440124/kyrie-low-3-eclipse-basketball-shoe-4VT0rq.jpg",
+        price:"$1100"
+    }
+  };
 
+  const useStyles = makeStyles({
+    root: {
+      maxWidth: 345,
+    },
+    media: {
+      height: 140,
+    },
+  });
 const Products = () => {
+    const classes=useStyles();
+    const navigate=useNavigate();
     return ( 
         <div className='row'>
-            <div className='col-8'>
-            <h3>Products</h3>
+            <div className='col-12 col-md-8'>
+            <h3>Shoes</h3>
+                <div className='row'>
+
+                    {Object.entries(shoes).map(([k,{name,img}])=>{
+                        return(
+                            <Card className='col-12 col-lg-6' variant='outlined' key={k} onClick={()=>{navigate(k)}}>
+                                <CardContent>
+                                    <h4>{name}</h4>
+                                    <img src={img} alt={name} style={{width:'24rem'}} />
+                                </CardContent>
+                            </Card>
+                        )
+                    })}
+                   
+               
+                </div>
             </div>
             <div className='col-4'>
                 <Outlet>
